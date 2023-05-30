5e1deb7d817930a29a0deda4b4a40d09b4ceeba5
César Guerra <warderer@gmail.com>
2023-05-29 21:09:20 -0600

    ✨ Componente Hola Mundo

---
+++ 01-vite-vanilla/src/components/HolaMundo.js
@@ -0,0 +1,8 @@
+const algo = 'Algo'
+
+const HolaMundo = () => `
+    <h1>Hola Mundo</h1>
+    <p>Este es un componente de prueba ${algo}</p>
+`
+
+export default HolaMundo
--- 01-vite-vanilla/src/main.js
+++ 01-vite-vanilla/src/main.js
@@ -1,21 +1,6 @@
 import './styles/style.css'
-import javascriptLogo from './assets/javascript.svg'
-import viteLogo from '/vite.svg'
+import HolaMundo from './components/HolaMundo'
 
 document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Secuencia de autodestrucción 04-24-220-sa-3213213
-    </p>
-  </div>
+    ${HolaMundo()}
 `
