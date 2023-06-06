68ab5384f17e2f8ecdd29ae8b57731a3484e64fd
DoloresPGz <dolores.pgz@gmail.com>
2023-06-05 19:51:51 -0600

    Iniciando proyecto TodoList

---
+++ 03-todo-list/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import TodoList from './components/TodoList'
+
+function App() {
+  
+  return (
+    <>
+      <TodoList />
+    </>
+  )
+}
+
+export default App
---
+++ 03-todo-list/src/components/TodoList.jsx
@@ -0,0 +1,9 @@
+const TodoList = () => {
+  return (
+    <div>
+        <h1>Lista de Tareas</h1>
+    </div>
+    
+  )
+}
+export default TodoList
