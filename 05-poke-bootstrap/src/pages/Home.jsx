b1c8885e584723fc3f5326e5221dbde4a6f79e3b
César Guerra <warderer@gmail.com>
2023-06-14 21:35:59 -0600

    ✨ Pokemons con Link

--- 05-poke-bootstrap/src/pages/Home.jsx
+++ 05-poke-bootstrap/src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   const [pokemons, setPokemons] = useState([])
@@ -34,7 +35,9 @@ const Home = () => {
               <div className='card'>
                 <img className='card-img-top' src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.url.split('/')[6]}.png`} alt={pokemon.name} />
                 <div className='card-body'>
-                  <h4 className='card-title'>{pokemon.name}</h4>
+                  <Link to={`/pokemon/${pokemon.url.split('/')[6]}`}>
+                    <h4 className='card-title'>{pokemon.name}</h4>
+                  </Link>
                 </div>
               </div>
             </div>
