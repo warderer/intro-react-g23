bd4501f8ab6987d2f76813bf7eeb18d486f570bd
César Guerra <warderer@gmail.com>
2023-06-14 21:13:41 -0600

    ✨ Navbar con NavLink

--- 05-poke-bootstrap/src/components/Navbar.jsx
+++ 05-poke-bootstrap/src/components/Navbar.jsx
@@ -1,14 +1,16 @@
+import { NavLink } from 'react-router-dom'
+
 const Navbar = () => {
   return (
     <nav className='navbar navbar-expand-sm navbar-light bg-warning d-flex justify-content-between px-4'>
-      <a className='navbar-brand' href='#'>Pokédex</a>
+      <NavLink className='navbar-brand' to='#'>Pokédex</NavLink>
 
       <ul className='navbar-nav mr-auto mt-2 mt-lg-0'>
         <li className='nav-item px-2'>
-          <a className='nav-link' href='/'>Home</a>
+          <NavLink className='nav-link' to='/'>Home</NavLink>
         </li>
         <li className='nav-item px-2'>
-          <a className='nav-link' href='/about'>Acerca de Pokémon</a>
+          <NavLink className='nav-link' to='/about'>Acerca de Pokémon</NavLink>
         </li>
       </ul>
 
