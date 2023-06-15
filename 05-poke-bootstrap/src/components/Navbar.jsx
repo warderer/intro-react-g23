f96f2b449692b7eed8e12b46738262c647da748d
César Guerra <warderer@gmail.com>
2023-06-14 19:35:34 -0600

    💄 Navbar con Bootstrap

--- 05-poke-bootstrap/src/components/Navbar.jsx
+++ 05-poke-bootstrap/src/components/Navbar.jsx
@@ -1,6 +1,18 @@
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='navbar navbar-expand-sm navbar-light bg-warning d-flex justify-content-between px-4'>
+      <a className='navbar-brand' href='#'>Pokédex</a>
+
+      <ul className='navbar-nav mr-auto mt-2 mt-lg-0'>
+        <li className='nav-item px-2'>
+          <a className='nav-link' href='/'>Home</a>
+        </li>
+        <li className='nav-item px-2'>
+          <a className='nav-link' href='/about'>Acerca de Pokémon</a>
+        </li>
+      </ul>
+
+    </nav>
   )
 }
 export default Navbar
