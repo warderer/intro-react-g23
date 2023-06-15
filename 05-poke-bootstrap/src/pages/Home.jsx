717a3eff6e4f0745948623b38b3f793afd41d3a3
César Guerra <warderer@gmail.com>
2023-06-14 20:10:10 -0600

    ✨ Buscador de Pokemons

--- 05-poke-bootstrap/src/pages/Home.jsx
+++ 05-poke-bootstrap/src/pages/Home.jsx
@@ -1,6 +1,47 @@
+import { useState, useEffect } from 'react'
+
 const Home = () => {
+  const [pokemons, setPokemons] = useState([])
+  const [searchTerm, setSearchTerm] = useState('')
+
+  useEffect(() => {
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+      .then(response => response.json())
+      .then(data => setPokemons(data.results))
+      .catch(error => console.log(error))
+  }, [])
+
+  const handleInputChange = event => {
+    setSearchTerm(event.target.value)
+  }
+
+  const filteredPokemons = pokemons.filter(pokemon => {
+    return pokemon.name.toLowerCase().includes(searchTerm.toLowerCase())
+  })
+
   return (
-    <h1>Home</h1>
+    <>
+      <div className='container'>
+        <h1>Pokédex</h1>
+
+        <form className='form-inline my-2 my-lg-0 w-75'>
+          <input type='text' className='form-control' id='search' placeholder='Enter name' value={searchTerm} onChange={handleInputChange} />
+        </form>
+
+        <div className='row'>
+          {filteredPokemons.map(pokemon => (
+            <div className='col-sm-4 mb-4' key={pokemon.name}>
+              <div className='card'>
+                <img className='card-img-top' src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.url.split('/')[6]}.png`} alt={pokemon.name} />
+                <div className='card-body'>
+                  <h4 className='card-title'>{pokemon.name}</h4>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
   )
 }
 export default Home
